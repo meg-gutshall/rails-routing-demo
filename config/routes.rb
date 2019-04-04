32e665fbce812aa0932ebb4fb8c22a64de593c8c
Gingertonic <bethmschofield@gmail.com>
2019-04-04 15:27:17 +0100

    for sg

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'welcome#home'
-  post '/say-something', to: 'welcome#say_something'
+  # post '/say-something', to: 'welcome#say_something'
+  get '/songs', to: 'songs#index'
+  resources :songs, only: [:index]
+
+  # No route matches [GET] "/songs"
+
 
 end
