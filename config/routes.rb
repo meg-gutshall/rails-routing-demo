3364bb55c562fc18ca383bfe9aae24d8994e9c7c
Meg Gutshall <meghan.gutshall@gmail.com>
2019-04-04 11:05:49 -0400

    Live discussion on routing study session

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/', to: 'welcome#home'
+  # get '/', to: 'welcome#home'
   # post '/say-something', to: 'welcome#say_something'
-  get '/songs', to: 'songs#index'
-  resources :songs, only: [:index]
+  # get '/songs', to: 'songs#index'
+  # get '/songs/:id', to: 'songs#show', as: 'song'
 
-  # No route matches [GET] "/songs"
+  resources :songs, except: [:show]
+
+  # artists/:id/songs/:id
+  resources :artists, only: [:index] do
+    resources :songs, only: [:show]
+  end
 
 
 end
